0fa022d532bab3a2cf494bd9de84834d80fd5b1a
pennt04 <pennt2004@gmail.com>
2024-04-24 06:34:24 +0100

    Week1, Add LUA to project

---
+++ LUA_SIClone/LuaHelper.h
@@ -0,0 +1,13 @@
+#pragma once
+
+
+#include <string>
+#include <functional>
+#include<map>
+
+extern "C"
+{
+#include "../lua-5.4.4/include/lua.h"
+#include "../lua-5.4.4/include/lauxlib.h"
+#include "../lua-5.4.4/include/lualib.h"
+}
